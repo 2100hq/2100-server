4118799cbd57e686fa3371b5404c82d40ab10fe7
daywiss <daywiss@gmail.com>
2019-09-09 14:57:32 -0400

    disable retry writes

--- libs/mongo/mongo.js
+++ libs/mongo/mongo.js
@@ -2,7 +2,7 @@ const assert = require('assert')
 const mongo = require('mongodb')
 module.exports = async config => {
   assert(config.uri, 'requires dburi')
-  const db = (await mongo.connect(config.uri, { useNewUrlParser: true })).db()
+  const db = (await mongo.connect(config.uri, {retryWrites:false, useNewUrlParser: true })).db()
   return db
 }
 
