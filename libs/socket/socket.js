066f1bee36aae81b25789f8bb9910b0c5d14b7d4
daywiss <daywiss@gmail.com>
2019-09-12 10:12:59 -0400

    remove auth error

--- libs/socket/socket.js
+++ libs/socket/socket.js
@@ -51,7 +51,7 @@ module.exports = async (config, libs,emit=x=>x) => {
       actions.auth(socket,action,...args).then(result=>{
         cb(null, result)
       }).catch(err=>{
-        console.log('auth error',err)
+        // console.log('auth error',err)
         if (cb) cb(err.message)
       })
     })
